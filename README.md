036ffd2e34941122bb390fccc8437e48c49945f2
RChamyGit <39716460+RChamyGit@users.noreply.github.com>
2020-07-09 21:53:09 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# python-mini-course
+ linkedin's mini python course I did in 07-2020
---
+++ conditionals_start.py
@@ -0,0 +1,19 @@
+# exemplo condicoes
+
+def main():
+    x, y = 100, 100
+
+    # conditional flow uses if, elif, else
+    if(x < y):
+        st = "x is less than y"
+    elif(x == y):
+        st = "x is the same as y"
+    else:
+        st = "x is greater than y"
+    print (st)
+
+    st="x is less than y" if (x<y) else "x is greater than or the same as"
+
+if __name__ == "__main__":
+    main()
+#python nao tem switch case
